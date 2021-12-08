f76b1826bad5e4338995740546ba8da9f9200654
TarikElha <www.elhantiti.t@gmail.com>
2021-12-08 16:30:08 +0100

    updated twig historic

--- src/Controller/HistoricController.php
+++ src/Controller/HistoricController.php
@@ -5,19 +5,19 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Order;
 
 class HistoricController extends AbstractController
 {
     #[Route('/historic', name: 'historic')]
     public function index(): Response
     {
-/*         $historics = $this->getDoctrine()
-             ->getRepository(Historic::class)
+         $orders = $this->getDoctrine()
+             ->getRepository(Order::class)
              ->findAll();
 
         return $this->render('historic/index.html.twig', [
-            'historics' => $historics,
-        ]); */
-        return $this->render('historic/index.html.twig');
+            'historics' => $orders,
+        ]);
     }
 }
