38f5802c3c5e87c47299382e8a7b01e826515467
Mario Rocha <mrocha51248@gmail.com>
2021-12-09 11:32:47 +0100

    Create ArticleType

---
+++ src/Form/ArticleType.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Article;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ArticleType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('width', null, ['label' => 'Largeur'])
+            ->add('length', null, ['label' => 'Longueur'])
+            ->add('quantity', null, ['label' => 'Quantité'])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Article::class,
+        ]);
+    }
+}
