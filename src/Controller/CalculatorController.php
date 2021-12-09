d04516dbeaa9f8a3eee8c5c30896aef7df2d9fc2
Mario Rocha <mrocha51248@gmail.com>
2021-12-09 16:46:29 +0100

    CalculatorController: method refactoring

--- src/Controller/CalculatorController.php
+++ src/Controller/CalculatorController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Order;
 use App\Form\CalculatorType;
 use DateTime;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -13,44 +14,34 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/', name: 'calculator_')]
 class CalculatorController extends AbstractController
 {
-    #[Route('/', name: 'index')]
-    public function index(Request $request): Response
+    #[Route('/', name: 'index', defaults: ['save' => false])]
+    #[Route('/save', name: 'save', defaults: ['save' => true])]
+    public function index(Request $request, EntityManagerInterface $entityManager, bool $save): Response
     {
         $order = new Order();
         $form = $this->createForm(CalculatorType::class, $order);
         $form->handleRequest($request);
-        $showResults = false;
-        if ($form->isSubmitted() && $form->get('articles')->isValid()) {
-            // TODO: calculate
-            // $order->setLength(...);
-            // $order->setLeftover(...);
-            $showResults = true;
+        $showResults = $save;
+        if ($form->isSubmitted()) {
+            if ($save && $form->isValid()) {
+                $order->setSavedAt(new DateTime());
+                foreach ($order->getArticles() as $article) {
+                    $entityManager->persist($article);
+                }
+                $entityManager->persist($order);
+                $entityManager->flush();
+                return $this->redirectToRoute('calculator_history');
+            }
+            if (!$save && $form->get('articles')->isValid()) {
+                // TODO: calculate
+                // $order->setLength(...);
+                // $order->setLeftover(...);
+                $showResults = true;
+            }
         }
         return $this->render('calculator/index.html.twig', [
             "form" => $form->createView(),
             "showResults" => $showResults,
-        ]);
-    }
-
-    #[Route('/save', name: 'save')]
-    public function save(Request $request): Response
-    {
-        $order = new Order();
-        $form = $this->createForm(CalculatorType::class, $order);
-        $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()) {
-            $order->setSavedAt(new DateTime());
-            $entityManager = $this->getDoctrine()->getManager();
-            foreach ($order->getArticles() as $article) {
-                $entityManager->persist($article);
-            }
-            $entityManager->persist($order);
-            $entityManager->flush();
-            return $this->redirectToRoute('calculator_history');
-        }
-        return $this->render('calculator/index.html.twig', [
-            "form" => $form->createView(),
-            "showResults" => true,
         ]);
     }
 }
