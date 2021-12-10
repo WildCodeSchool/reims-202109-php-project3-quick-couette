c222054e4d2a98ce7979992c0cee97a3bdb1328d
Mario Rocha <mrocha51248@gmail.com>
2021-12-10 10:32:10 +0100

    CalculatorController: let client handle calculations

--- src/Controller/CalculatorController.php
+++ src/Controller/CalculatorController.php
@@ -14,34 +14,23 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/', name: 'calculator_')]
 class CalculatorController extends AbstractController
 {
-    #[Route('/', name: 'index', defaults: ['save' => false])]
-    #[Route('/save', name: 'save', defaults: ['save' => true])]
-    public function index(Request $request, EntityManagerInterface $entityManager, bool $save): Response
+    #[Route('/', name: 'index')]
+    public function index(Request $request, EntityManagerInterface $entityManager): Response
     {
         $order = new Order();
         $form = $this->createForm(CalculatorType::class, $order);
         $form->handleRequest($request);
-        $showResults = $save;
-        if ($form->isSubmitted()) {
-            if ($save && $form->isValid()) {
-                $order->setSavedAt(new DateTime());
-                foreach ($order->getArticles() as $article) {
-                    $entityManager->persist($article);
-                }
-                $entityManager->persist($order);
-                $entityManager->flush();
-                return $this->redirectToRoute('calculator_history');
+        if ($form->isSubmitted() && $form->isValid()) {
+            $order->setSavedAt(new DateTime());
+            foreach ($order->getArticles() as $article) {
+                $entityManager->persist($article);
             }
-            if (!$save && $form->get('articles')->isValid()) {
-                // TODO: calculate
-                // $order->setLength(...);
-                // $order->setLeftover(...);
-                $showResults = true;
-            }
+            $entityManager->persist($order);
+            $entityManager->flush();
+            return $this->redirectToRoute('calculator_history');
         }
         return $this->render('calculator/index.html.twig', [
             "form" => $form->createView(),
-            "showResults" => $showResults,
         ]);
     }
 }
