95d6cf6adc0af31f2100ef4a553e7bf0b2e6f31a
TarikElha <www.elhantiti.t@gmail.com>
2021-12-15 09:36:26 +0100

    view of historic

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -7,17 +7,16 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Order;
 
-class HistoricController extends AbstractController
+class OrderController extends AbstractController
 {
-    #[Route('/historic', name: 'historic')]
+    #[Route('/order', name: 'order')]
     public function index(): Response
     {
          $orders = $this->getDoctrine()
              ->getRepository(Order::class)
              ->findAll();
-
-        return $this->render('historic/index.html.twig', [
-            'historics' => $orders,
+        return $this->render('order/index.html.twig', [
+            'orders' => $orders,
         ]);
     }
 }
