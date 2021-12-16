36464ae0c2295ff48c2879540f252991108d0425
kbardy <k.bardy@laposte.net>
2021-12-16 14:24:06 +0100

    change articleCount line 14 on ArticleFixtures.php

--- src/DataFixtures/ArticleFixtures.php
+++ src/DataFixtures/ArticleFixtures.php
@@ -11,7 +11,7 @@ class ArticleFixtures extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager): void
     {
-        $articleCount = 0;
+        $articleCount = 10;
         for ($i = 0; $i < $articleCount; $i++) {
             $article = new Article();
             $article->SetName('housse');
