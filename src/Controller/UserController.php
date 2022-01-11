6c59814b32f1e6c1f7f49a0d31914bf3c1715475
TarikElha <www.elhantiti.t@gmail.com>
2022-01-11 17:10:59 +0100

    updated profil page

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -10,11 +10,10 @@ use App\Entity\User;
 #[Route('/profil', name: 'user_')]
 class UserController extends AbstractController
 {
-    #[Route('/{user<^[0-9]+$>}', name: '_show')]
-    public function show(User $user): Response
+    #[Route('/', name: 'show')]
+    public function show(): Response
     {
         return $this->render('user/show.html.twig', [
-            'user' => $user,
         ]);
     }
 }
