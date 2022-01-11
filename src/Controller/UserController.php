4a5ed613fb22a3132f495c969537f618833c603f
TarikElha <www.elhantiti.t@gmail.com>
2022-01-11 16:09:10 +0100

    created userControler and view

---
+++ src/Controller/UserController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\User;
+
+#[Route('/profil', name: 'user_')]
+class UserController extends AbstractController
+{
+    #[Route('/{user<^[0-9]+$>}', name: '_show')]
+    public function show(User $user): Response
+    {
+        return $this->render('user/show.html.twig', [
+            'user' => $user,
+        ]);
+    }
+}
