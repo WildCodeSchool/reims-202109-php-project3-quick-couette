71aed73d3b849aa5477a5d3c41051afa42cd3da8
mrocha51248 <87133024+mrocha51248@users.noreply.github.com>
2022-01-13 16:53:24 +0100

    Tests: create CalculatorControllerTest

---
+++ tests/Controller/CalculatorControllerTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class CalculatorControllerTest extends WebTestCase
+{
+    /**
+     * @dataProvider getHistoryPageLinks
+     */
+    public function testHistoryPageLinks(
+        int $pageRequest,
+        int $expectedMin,
+        int $expectedMax,
+        ?int $expectedActive
+    ): void {
+        $client = static::createClient();
+        $crawler = $client->request('GET', "/history?page=$pageRequest");
+
+        $this->assertResponseIsSuccessful();
+
+        $expectedPage = $expectedMin - 1;
+        $children = $crawler->filter('.history-pages')->children();
+        foreach ($children as $child) {
+            ++$expectedPage;
+            $this->assertEquals($child->nodeValue, "$expectedPage");
+            $this->assertEquals($child->nodeName, $expectedPage === $expectedActive ? 'span' : 'a');
+        }
+        $this->assertEquals($expectedPage, $expectedMax);
+    }
+
+    public function getHistoryPageLinks(): iterable
+    {
+        return [
+            [-1, 1, 11, 1],
+            [1, 1, 11, 1],
+            [5, 1, 11, 5],
+            [6, 1, 11, 6],
+            [7, 2, 12, 7],
+            [29, 24, 34, 29],
+            [30, 25, 35, 30],
+            [32, 25, 35, 32],
+            [33, 25, 35, 33],
+            [100, 25, 35, null],
+        ];
+    }
+}
