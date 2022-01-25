d568316688b281b862dd12b900696017be73bfa1
mrocha51248 <87133024+mrocha51248@users.noreply.github.com>
2022-01-25 11:10:11 +0100

    define articles in StoreController

--- src/Controller/StoreController.php
+++ src/Controller/StoreController.php
@@ -8,6 +8,24 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class StoreController extends AbstractController
 {
+    public const ARTICLES = [
+        [
+            'name' => 'King size',
+            'length' => 260,
+            'width' => 240,
+        ],
+        [
+            'name' => 'Double standard',
+            'length' => 240,
+            'width' => 220,
+        ],
+        [
+            'name' => 'Oreiller',
+            'length' => 60,
+            'width' => 60,
+        ],
+    ];
+
     #[Route('/store', name: 'store')]
     public function index(): Response
     {
