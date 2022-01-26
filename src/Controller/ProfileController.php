e52c024599ca53c9b4a5f1be9121ea6a6f0f8563
kbardy <k.bardy@laposte.net>
2022-01-26 10:58:23 +0100

    add findByUser

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -7,15 +7,21 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\User;
+use App\Repository\OrderRepository;
 
 #[Route('/profile', name: 'profile_')]
 class ProfileController extends AbstractController
 {
     #[Route('/', name: 'index')]
     #[IsGranted('IS_AUTHENTICATED_FULLY')]
-    public function index(): Response
+    public function index(OrderRepository $orderRepository): Response
     {
+        /** @var User $user */
+        $user = $this->getUser();
+        $orders = $orderRepository->findByUser($user);
+
         return $this->render('profile/index.html.twig', [
+            "orders" => $orders,
         ]);
     }
 }
